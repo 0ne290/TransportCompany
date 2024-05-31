e2a87b89f7cb4501122bbc2bf9c97842d447d87c
One290 <lenya.dorovskoy@mail.ru>
2024-05-31 12:49:33 +0800

    yighop

--- Web/Controllers/LoginController.cs
+++ Web/Controllers/LoginController.cs
@@ -26,7 +26,7 @@ public class LoginController(UserInteractor userInteractor) : Controller
     // В идеале часть логики входа должна быть на фронтенде, чтобы в случае ввода неправильных логина и/или пароля происходило встраивание с помощью AJAX в тот же документ на той же странице сообщения о неправильном вводе
     [HttpPost]
     [Route("user")]
-    public async Task<IActionResult> PostUser(string login, string password)// Тогда действия входа должны возвращать 200 или 400. Если 200, то JavaScript-код на клиенте сам выполнит переадресацию. Если 400 - встроит в документ сообщение о неправильном вводе
+    public async Task<IActionResult> PostUser(string login, string password, string remember)// Тогда действия входа должны возвращать 200 или 400. Если 200, то JavaScript-код на клиенте сам выполнит переадресацию. Если 400 - встроит в документ сообщение о неправильном вводе
     {
         if (!await userInteractor.Login(login, password))
             return BadRequest();
@@ -34,15 +34,24 @@ public class LoginController(UserInteractor userInteractor) : Controller
         var claims = new[] { new Claim(ClaimTypes.Name, login), new Claim(ClaimTypes.Role, "User") };
         var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
         var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+        AuthenticationProperties? authProperties = null;
+        if (remember == "yes")
+            authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                IsPersistent = true
+            };
+        else if (remember != "no")
             
-        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, authProperties);
             
         return Redirect("/user");
     }
     
     [HttpPost]
     [Route("administrator")]
-    public async Task<IActionResult> PostAdministrator(string login, string password)
+    public async Task<IActionResult> PostAdministrator(string login, string password, string remember)
     {
         if (login != "nimda" || password != "4202drowssap")
             return BadRequest();
